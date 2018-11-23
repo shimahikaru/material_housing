c75b224d3823038deda3cac1b36ec094f302766c
shimahikaru <i12007ushio@gmail.com>
2018-11-23 16:09:23 +0900

    edit brand comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,16 @@
 class CommentsController < ApplicationController
 
 def index
+  @comments = Comment.all
 end
 
 def show
+  @comment = Comment.find(params[:id])
 end
 
 def new
+  @comment = Comment.new
+  @brands = Brand.select(:id, :name)
 end
 
 def create
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,6 +12,7 @@ Rails.application.config.assets.precompile += %w( companies.css )
 Rails.application.config.assets.precompile += %w( about.css )
 Rails.application.config.assets.precompile += %w( brands.css )
 Rails.application.config.assets.precompile += %w( brandshow.css )
+Rails.application.config.assets.precompile += %w( comments.css )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
