b8ed1420648e3309d5928123e91240ca39db5e2d
shimahikaru <i12007ushio@gmail.com>
2018-11-25 18:27:57 +0900

    edit mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,16 +90,17 @@ Rails.application.configure do
   end
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: host }
+  host = 'homematerial.herokuapp.com'
   config.action_mailer.smtp_settings = {
   :enable_starttls_auto => true,
   :address => "smtp.gmail.com",
   :port => 587,
+  :authentication => :plain,
   :domain => 'smtp.gmail.com',
   :user_name => "techcamp69shima@gmail.com", #gmailアドレス
   # :password => "techcamp69", #gmailパスワード
   :password => 'evdzrkgcyhboemlj',
-  :authentication => 'login',
 }
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
