9a828546d67bacad83d62c62e568040194450550
shimahikaru <i12007ushio@gmail.com>
2018-11-27 08:47:11 +0900

    edit ckeditor

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module MaterialHousing
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+# config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+# config.assets.precompile += Ckeditor.assets
+# config.assets.precompile += %w(ckeditor/*)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
