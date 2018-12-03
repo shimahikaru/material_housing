fb1a2b221fd47b4dac2e455b6e1331e43a4f9f08
shimahikaru <i12007ushio@gmail.com>
2018-12-03 23:43:54 +0900

    edit type_column

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,6 +12,7 @@ Rails.application.configure do
   config.assets.precompile += Ckeditor.assets
   config.assets.precompile += %w( ckeditor/* )
   config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+  config.assets_languages = ['ja', 'fr']
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
