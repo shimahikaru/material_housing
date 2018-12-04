da9bf04cecb3ea00e15df7b3be8922699f67a1ec
shimahikaru <i12007ushio@gmail.com>
2018-12-04 22:14:36 +0900

    edit migrate

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -4,7 +4,7 @@ class TopController < ApplicationController
     @companies_count = Company.select(:id).count
     @image = Brandimage.group(:brand_id)
     @brands = Brand.select(:id, :name, :location, :grade, :text, :price, :company_id).where(location: 1).limit(4)
-    @rest_brands = Brand.select(:id, :name, :location, :grade, :text, :price, :company_id).where(location: 5).limit(4)
-    @rava_brands = Brand.select(:id, :name, :location, :grade, :text, :price, :company_id).where(location: 3).limit(4)
+    @rest_brands = Brand.select(:id, :name, :location, :grade, :text, :price, :company_id).where(location: 3).limit(4)
+    @rava_brands = Brand.select(:id, :name, :location, :grade, :text, :price, :company_id).where(location: 5).limit(4)
   end
 end
