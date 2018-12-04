4f6b332a86439a8edb3a517ec40f2a035b63e82c
shimahikaru <i12007ushio@gmail.com>
2018-12-04 19:33:01 +0900

    edit migrate

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,5 +2,7 @@ class TopController < ApplicationController
   def index
     @companies = Company.limit(20)
     @companies_count = Company.select(:id).count
+    @image = Brandimage.group(:brand_id)
+    @brands = Brand.select(:id, :name, :location, :grade, :text, :price, :company_id).where(location: 1).limit(4)
   end
 end
